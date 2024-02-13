57f13bd0bffd5d101e65fa030cbd4cd20cad3d97
Gustavo Bremm <gustavorino@gmail.com>
2024-02-13 16:17:53 -0300

    get vars from env

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,14 +1,14 @@
+import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'sanity-meetup-game',
 
-  projectId: 'a67i5yzg',
-  dataset: 'production',
+  projectId: process.env.VITE_SANITY_PROJECT_ID || 'not-set',
+  dataset: process.env.VITE_SANITY_DATASET || 'production',
 
   plugins: [deskTool(), visionTool()],
 
